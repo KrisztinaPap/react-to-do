d524a3445e3826b69b8693fea9571378803874e3
Krisztina Pap <krisztina.pap@outlook.com>
2020-09-11 13:41:10 -0600

    Adds uuid via npm and unique ids for tasks;

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,19 +1,20 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function ToDo () 
 {
     const [ newTask, setNewTask ] = useState( '' );
 
     const [ toDos, setToDos ] = useState( [
-        { task: "Buy milk" },
-        { task: "Learn React" },
-        { task: "Find out what Redux is" }
+        { id: uuidv4(), task: "Buy milk" },
+        { id: uuidv4(), task: "Learn React" },
+        { id: uuidv4(), task: "Find out what Redux is" }
     ] );
 
 const addNewTask = event => {
     event.preventDefault();
     const newToDosList = [...toDos];
-    newToDosList.push( { task: newTask } );
+    newToDosList.push( { id: uuidv4(), task: newTask } );
     setToDos( newToDosList );
     setNewTask( '' ); // Clear input field
 }
@@ -34,7 +35,7 @@ const addNewTask = event => {
                 </p>
                 <input type="submit" value="Add To-Do" />
             </form>
-            <ul>{toDos.map( (toDo, index) => <li key={index}>{toDo.task}</li> )}</ul>
+            <ul>{toDos.map( toDo => <li key={toDo.id}>{toDo.task}</li> )}</ul>
         </>
     );
 }
